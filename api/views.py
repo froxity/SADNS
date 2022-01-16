920b6047f752e4ead0055f1c3f0abe3dc19b1290
Ahmad Afiq <ahmad_afiq53@yahoo.com>
2022-01-17 01:49:50 +0800

    Update and Add new API logic
    Create profileConfig[GET & PUT], Whitelist & Blacklist [GET], and adjust Domains[POST]

--- api/views.py
+++ api/views.py
@@ -9,30 +9,104 @@ from dashboard.models import *
 @permission_classes([IsAuthenticated])
 def domains(request):
   if request.method == 'GET':
-    domains = Domain.objects.all()
+    profile = request.user.profile
+    # Get all domains list
+    domains = Domain.objects.all().filter(owner=profile)
+    # Serialize JSON data
     serializer = DomainSerializer(domains, many=True)
     return Response(serializer.data)
 
   elif request.method == 'POST':
-    user = request.user.profile
+    # Request user profile since we need to get all domain from that User
+    profile = request.user.profile
+    # Put all POST data into variable
     data = request.data
+    # Get which category id that need to be used
     cat = Category.objects.get(id=data['cat_id'])
-    domain = Domain.objects.create(
-      owner=user,
-      domain=data['domain'],
-      freq = data['freq'],
-      cat_id=cat
-    )
-    domain.save()
+    # Get all domain list
+    tempdomain_list = profile.domain_set.all()
+    # Put POST data into temporary variable
+    tempdomain_data = data['domain']
+    # Declare domainstatus to be added into Domain Model
+    domainstatus = False
+    # Checking is it POST data is inside tempdomain_list
+    for x in tempdomain_list:
+      if tempdomain_data in x.domain:
+         item = Domain.objects.get(id=x.id)
+         item.freq = item.freq + data['freq']
+         item.save()
+         domainstatus = True
+         break
+    # If is not inside tempdomain_list then Create new domain into Domain Model
+    if domainstatus == False:
+      domain = Domain.objects.create(
+        owner = profile,
+        domain = data['domain'],
+        freq = data['freq'],
+        cat_id = cat
+      )
+      domain.save()
+    
     serializer = DomainSerializer(data=request.data)
+    # Checking POST data is CREATED or BAD REQUEST
     if serializer.is_valid():
-        serializer.save()
+        # serializer.save() # not applicable since we do not create new POST data from serializer
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getCategory(request):
+  # Get all category list
   category = Category.objects.all()
+  # Serialize JSON data
   serializer = CategorySerializer(category, many=True)
-  return Response(serializer.data)
+  return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getWhitelist(request):
+  # Request user profile since we need to get all whitelist from that User
+  profile = request.user.profile
+  # Get all whitelist domain from the USER
+  domain_whitelist = profile.whitelist_set.all()
+  # Serialize JSON data
+  serializer = WhitelistSerializer(domain_whitelist, many=True)
+  return Response(serializer.data ,status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getBlacklist(request):
+  # Request user profile since we need to get all blacklist from that User
+  profile = request.user.profile
+  # Get all blacklist domain from the USER
+  domain_blacklist = profile.blacklist_set.all()
+  # Serialize JSON data
+  serializer = BlacklistSerializer(domain_blacklist, many=True)
+  return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getProfileConfig(request):
+  # Request user profile
+  profile = request.user.profile
+  settings = profileConfig.objects.all().filter(owner=profile)
+  serializer = profileConfigSerializer(settings, many=True)
+  return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def putWebFilter(request, pk):
+  """Update webfilter status"""
+  try:
+    setting = profileConfig.objects.get(id=pk)
+  except setting.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  serializer = profileConfigSerializer(setting, data=request.data)
+  if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  
