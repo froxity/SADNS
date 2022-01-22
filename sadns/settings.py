ff3068c9719a0e215a0173fe013c72ac578ff937
Ahmad Afiq <ahmad_afiq53@yahoo.com>
2022-01-22 17:50:45 +0800

    Update collecstatic

--- sadns/settings.py
+++ sadns/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-3s_sgx3zxv7jkd%)=_s@160qj3q9k$(i6heq-vpvzko6-h0a)w
 
 # Debug and development process use This!
 DEBUG = True
-ALLOWED_HOSTS = [sadns.herokuapp.com]
+ALLOWED_HOSTS = ['sadns.herokuapp.com']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Uncomment this for if want to use POST website into liver server
