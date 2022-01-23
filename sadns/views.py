6b1e34def188aa2568285b9b06f15ade9a34a993
Ahmad Afiq <ahmad_afiq53@yahoo.com>
2022-01-23 11:25:58 +0800

    Reactivate handler500

--- sadns/views.py
+++ sadns/views.py
@@ -9,5 +9,5 @@ def handler403(request, exception):
 def handler404(request, exception):
   return render(request, "httpresponse/http404.html", status=404)
 
-# def handler500(request):
-#   return render(request, "httpresponse/http500.html", status=500)
+def handler500(request):
+  return render(request, "httpresponse/http500.html", status=500)
